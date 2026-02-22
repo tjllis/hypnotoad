ffc69cabf7c3c944a2e4d43d85010616260e05a5
tjllis <floydjellis@gmail.com>
2026-02-22 21:05:29 +0100

    Initial commit: React TypeScript application with Vite

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import toad from "./assets/toad.png";
+import style from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={`${style.viewport} flex-center`}>
+      <div className={`${style.hypnoContainer} flex-center`}>
+        <div className={style.circles} />
+        <div className={`${style.toadContainer} flex-center`}>
+          <img src={toad} alt="Toad image" />
+          <div className={style.leftEye}>
+            <div className={style.pupil} />
+          </div>
+          <div className={style.rightEye}>
+            <div className={style.pupil} />
+          </div>
+          <button
+            onClick={() =>
+              window.open(
+                "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                "_blank",
+              )
+            }
+          >
+            Accept me to JetBrains
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+//https: vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    devSourcemap: true,
+  },
+  build: {
+    sourcemap: "inline",
+    cssCodeSplit: true,
+    // minify: false,
+    assetsInlineLimit: 0,
+  },
+});
